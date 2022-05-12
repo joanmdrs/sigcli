4d7fb49cb36f1071da90649760cfa8debea5a45c
Adson <adson.matheus62@gmail.com>
2022-05-12 20:32:29 -0300

    feat: update do doctor usando prisma #43

--- server/repositories/doctorRepository.js
+++ server/repositories/doctorRepository.js
@@ -9,3 +9,18 @@ export const createDoctor = async (doctor) => {
 export const findManyDoctors = async () => {
   return await prisma.doctor.findMany();
 };
+
+export const updateDoctorWithPrisma = async (doctor) => {
+  const { id, name, crm, username, password } = doctor;
+  return await prisma.doctor.update({
+    where: {
+      id: Number(id),
+    },
+    data: {
+      name,
+      crm,
+      username,
+      password,
+    },
+  });
+};
