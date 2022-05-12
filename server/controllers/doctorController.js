09abc41c451f784acf2f9ceaf0f4c40faf70a371
Adson <adson.matheus62@gmail.com>
2022-05-12 20:19:53 -0300

    feat: controller getDoctors #43

--- server/controllers/doctorController.js
+++ server/controllers/doctorController.js
@@ -1,7 +1,14 @@
-import { createDoctor } from "../repositories/doctorRepository.js";
+import {
+    createDoctor,
+    findManyDoctors
+} from "../repositories/doctorRepository.js";
 
 export const registerDoctor = async (req, res) => {
     const doctorBody = req.body;
     const doctor = await createDoctor(doctorBody);
     res.json(doctor);
+};
+export const getDoctors = async (req, res) => {
+    const doctors = await findManyDoctors();
+    res.json(doctors);
 };
