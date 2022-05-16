847e215a0135ae90164ca6b25a81c26e642a38dd
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-05-16 20:29:33 -0300

    Implementando o formulário da entidade Exam #44

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Home from "./pages/Home/Home"
+import Exam from "./pages/Exam/Exam"
 import Recepcionist from "./pages/Recepcionist/Recepcionist"
 
 import {BrowserRouter, Routes as Switch, Route } from "react-router-dom"
@@ -10,6 +11,7 @@ export default function Routes () {
         <BrowserRouter>
             <Switch>
                 <Route path="/" element={<Home />} />
+                <Route path="/exams" element={<Exam />} />
                 <Route path="/recepcionist" element={<Recepcionist />} />
             </Switch>
         </BrowserRouter>
