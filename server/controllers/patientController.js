ed059712439da57ff2af03cf70901c37c9c2428d
Allan Gabriel <43339727+allangbr@users.noreply.github.com>
2022-05-18 20:31:37 -0300

    Implementando Controller do Patient #42

---
+++ server/controllers/patientController.js
@@ -0,0 +1,30 @@
+import {
+  createPatient,
+  listPatient,
+  updatePatientWithPrisma,
+  deletePatientWithPrisma
+} from '../repositories/patientRepository'
+
+export const registerPatient = async (req, res) => {
+  const patientBody = req.body
+
+  const patient = await createPatient(patientBody)
+  res.json(patient)
+}
+
+export const listPatients = async (req, res) => {
+  const listPatients = await listPatient()
+  return res.status(200).json(listPatients)
+}
+
+export const updatePatient = async (req, res) => {
+  const patient = req.body
+  const updatedPatient = await updatePatientWithPrisma(patient)
+  res.json(updatedPatient)
+}
+
+export const deletePatient = async (req, res) => {
+  const cpf = req.params.cpf
+  const deletedPatient = await deletePatientWithPrisma(cpf)
+  res.json(deletedPatient)
+}
