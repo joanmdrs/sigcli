80e252d74a3974d4f0ba526356afffafef658b4d
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-05-18 21:31:29 -0300

    definindo que a api utilize as rotas da entidade exam #44

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { recepcionistRoutes } from "./recepcionistRoutes.js";
+import { examRoutes } from "./examRoutes.js";
 export const apiRoutes = Router();
 
 apiRoutes.use("/recepcionist", recepcionistRoutes);
+apiRoutes.use("/exam", examRoutes);
