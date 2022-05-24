9717fdb2c31edb6974bdcbedf53fa3cdbb81f628
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2022-05-24 00:01:18 -0300

    Definindo as rotas da entidade Appointment para a api #41

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { recepcionistRoutes } from "./recepcionistRoutes.js";
+import { appointmentsRoutes } from "./appointmentsRoutes.js";
 export const apiRoutes = Router();
 
 apiRoutes.use("/recepcionist", recepcionistRoutes);
+apiRoutes.use("/appointment", appointmentsRoutes);
