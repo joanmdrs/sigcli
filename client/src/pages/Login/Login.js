99e7d85b431e8bf81f0009e4f1790758b15a0ebb
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-06-04 00:12:26 -0300

    implementando a interface do login #114

---
+++ client/src/pages/Login/Login.js
@@ -0,0 +1,88 @@
+import "./Login.css";
+import React, { Fragment } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import img from "../../assets/img/image-home.svg";
+import {
+    FormGroup,
+    Label,
+    Input,
+    Button
+} from 'reactstrap'
+
+
+export default function Login() {
+
+    return (
+        <div className="grid-container-primary">
+            <div className="box-top"></div>
+            <div className="box-bottom"></div>
+            <div className="grid-container-secondary">
+                <div className="box-left">
+                    <h3 className="text-align">Sign in</h3>
+                    <FormGroup>
+                        <Label 
+                            for="username">
+                            username or e-mail
+                        </Label>
+                        <Input 
+                            id="username" 
+                            name="username" 
+                            className="input username"
+                            type="text"
+                        ></Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label 
+                            for="password">
+                            password
+                        </Label>
+                        <Input
+                            id="password"
+                            name="password"
+                            className="input password"
+                            type="password"
+                        ></Input>
+                        <span>forgot password ?</span>
+                    </FormGroup>
+                    <FormGroup>
+                        <Input
+                            id="remember-me"
+                            name="remember-me"
+                            className="input checkbox"
+                            type="checkbox"
+                        ></Input>
+                        Remember me
+                    </FormGroup>
+                    <Button 
+                        id="button-sign-in"
+                        name="button-sign-in"
+                        className="button-sign-in"
+                        type="submit"> 
+                        Sign in
+                    </Button>
+                    <p className="text-align"> 
+                        D'ont have an account ? 
+                        <span> Sign up</span>
+                    </p>
+                </div>
+                <div className="box-right">
+                    <div className="container-img">
+                        <img src={img} alt="ilustration-doctors" />
+                    </div>
+                    <div className="container-text">
+                        <h3 className="text-align">Schedule your appointments</h3>
+                        <p className="text-align">
+                        Lorem ipsum dolor sit amet, consectetur 
+                        adipiscing elit, sed do.
+                        </p>
+                    </div>
+                    <div className="grid-container-third">
+                        <div className="item-1"></div>
+                        <div className="item-2"></div>
+                        <div className="item-3"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
--- client/src/routes.js
+++ client/src/routes.js
@@ -6,12 +6,14 @@ import Appointment from "./pages/Appointment/Appointment"
 import Recepcionist from "./pages/Recepcionist/Recepcionist"
 import Doctor from "./pages/Doctor/Doctor"
 import {BrowserRouter, Routes as Switch, Route } from "react-router-dom"
+import Login from "./pages/Login/Login";
 
 export default function Routes () {
 
     return (
         <BrowserRouter>
             <Switch>
+                <Route path="/login" element={<Login />} />
                 <Route path="/" element={<Home />} />
                 <Route path="/patient" element={<Patient />} />
                 <Route path="/exams" element={<Exam />} />
