0c524b2e042d83f92bd7602d77f02a01971b9bf1
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-06-15 16:45:34 -0300

    Chamando a função de criptografia da senha do user #108

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,11 +1,13 @@
-import { Role } from "@prisma/client";
 import { createUser } from "../repositories/userRepository.js";
+import { hashPassword } from "../service/cryptoService.js";
 
 export const registerUser = async (req, res) => {
     const {username, password, role} = req.body;
 
-    console.log(username);
-    const userBody = { username, password, role};
+    const userBody = { 
+        username: username.trim(), 
+        password: hashPassword(password), 
+        role};
     const user = await createUser(userBody);
     res.status(200).json(user);
 }
