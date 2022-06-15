55e0faf76a96e1601707ee480ae2d3838a687cbb
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-06-15 11:45:40 -0300

    Implementando função que verifica a autorização do usuário #108

--- server/service/middlewares/recepcionist.middleware.js
+++ server/service/middlewares/recepcionist.middleware.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+
 export const verifyJwtRecepcionist = (req, res, next) => {
   const token = req.headers["authorization"]; // Capturando o cabeçalho de autenticação com o token
 
@@ -12,3 +13,12 @@ export const verifyJwtRecepcionist = (req, res, next) => {
     res.status(400).json({ msg: "O Token é inválido!" });
   }
 };
+
+export const verifyAuthorization = (req, res) => {
+  const user = req.body;
+  // Verifica se o usuário possui permissão
+  if(user.role == Role.ADMIN || user.role == Role.RECEPCIONIST) {
+    return res.status(200).json({msg: "Usuário possui autorização de acesso"});
+  } 
+  return res.status(401).json({msg: "Usuário não possui autorização de acesso"});
+}
