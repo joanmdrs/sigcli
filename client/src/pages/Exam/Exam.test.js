7515de69a6171def6812240c77a9a5fc72dc66fc
Joan Medeiros <77677763+joanmdrs@users.noreply.github.com>
2022-06-21 17:42:43 -0300

    corrigindo erro no código #139

--- client/src/pages/Exam/Exam.test.js
+++ client/src/pages/Exam/Exam.test.js
@@ -4,7 +4,7 @@ import Exam from "./Exam";
 it('testando a renderização a page Exam', () => {
   const component = renderer.create(Exam);
   let tree = component.toJSON();
-  expect(tree).toMatchSnapshot(); tree = component.toJSON();
+  expect(tree).toMatchSnapshot(); 
 
   
 })
