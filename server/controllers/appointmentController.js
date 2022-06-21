dbf876435913c7fc71766bf34c1e4af275824328
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-06-20 23:56:20 -0300

    Modificando o crud de appointment para guardar um campo hora, e implementando as buscas por cpf e crm no form #81

--- server/controllers/appointmentController.js
+++ server/controllers/appointmentController.js
@@ -5,12 +5,46 @@ import {
     deleteAppointmentWithPrisma
 } from "../repositories/appointmentRepository.js";
 
+import { validateDate } from "../service/validations.js";
+
 
 export const registerAppointment = async (req, res) => {
-    const appointmentBody = req.body
-    const appointment = await createAppointment(appointmentBody)
-    res.json(appointment);
+
+   
+    const {
+        title, 
+        patient_cpf, 
+        patient_name, 
+        doctor_crm, 
+        doctor_name, 
+        data_appointment, 
+        hour_appointment, 
+        description} = req.body;
+
+    if(!validateDate(data_appointment)){
+        return res.status(406).json({ msg: 'Formato de Data Inválido!' })
+    }
+
+    try {
+        const appointmentBody = {
+            title: title.toLowerCase().trim(),
+            patient_cpf: patient_cpf.trim(),
+            patient_name: patient_name,
+            doctor_crm: doctor_crm,
+            doctor_name: doctor_name,
+            data_appointment: data_appointment,
+            hour_appointment: hour_appointment,
+            description: description
+        }
+        const appointment = await createAppointment(appointmentBody)
+        res.status(200).json(appointment);
+    } catch (error) {
+        res.status(500).json({ msg: 'Error no servidor! Procure o administrador!' })
+    }
+    
 };
+
+
 
 export const listAppointment = async (req, res) => {
     const appointmentList = await listAppointmentWithPrisma();
