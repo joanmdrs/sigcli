d22f4f4fed8c21308538612df079ed189d50d61c
Allan Gabriel <allanalmeidagbr@gmail.com>
2022-06-22 00:11:24 -0300

    atualizando teste

--- client/src/pages/Patient/Patient.test.js
+++ client/src/pages/Patient/Patient.test.js
@@ -1,7 +1,7 @@
 import { filterPatient } from '../../services/patientServices';
 
 test("resultados devem ser idênticos", async () => {
-  const data = await filterPatient('11058118404')
+  const data = await filterPatient('13064465465')
   const patients = JSON.parse(data)
-  expect(patients.name).toBe('allan')
+  expect(patients.name).toBe('joan de azevedo medeiros')
 })
