c7fb24a2410c52758b3f7c24efb0884b7a0686f9
Rauan Meirelles <77637967+rauan-meirelles@users.noreply.github.com>
2022-06-22 22:24:53 -0300

    Finalizando Teste unitário de listagem de consulta #140

--- client/src/components/ListAppointment/ListAppointment.test.js
+++ client/src/components/ListAppointment/ListAppointment.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { act } from "react-dom/test-utils";
-import {ListAppointment} from "./ListAppointment";
+import ListAppointment from "./ListAppointment";
 
 globalThis.IS_REACT_ACT_ENVIRONMENT = true;
 
@@ -20,15 +20,16 @@ it("returns the appointment patient's name", () => {
   const root = createRoot(container);
   const appointment = [
     {
-      title: "consulta",
-      patient_cpf: "13064465465",
-      patient_name: "joan de azevedo medeiros",
-      data_appointment: "2022-06-22",
-      doctor_name: "flavius gorgonio",
+      "patient_name":"joan de azevedo medeiros",
+      "name":"consulta",
+      "patient_cpf":"13064465465",
+      "doctor_name":"flavius gorgonio",
     },
   ];
+
   act(() => {
     root.render(<ListAppointment appointments={appointment} />);
   });
   expect(container.textContent).toContain("joan de azevedo medeiros");
+
 });
