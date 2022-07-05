d6cdd058c611fb0bc49fddd53dd624447c685347
Allan Gabriel <allanalmeidagbr@gmail.com>
2022-07-05 13:40:54 -0300

    Implementando teste de renderização da page Patient #173

--- client/src/pages/Patient/Patient.test.js
+++ client/src/pages/Patient/Patient.test.js
@@ -1,7 +1,16 @@
 import { filterPatient } from '../../services/patientServices';
+import renderer from 'react-test-renderer';
+import Patient from "./Patient";
 
 test("resultados devem ser idênticos", async () => {
   const data = await filterPatient('13064465465')
   const patients = JSON.parse(data)
   expect(patients.name).toBe('joan de azevedo medeiros')
+}) 
+
+
+it('testando a renderização a page Patient', () => {
+  const component = renderer.create(Patient);
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot(); 
 })
