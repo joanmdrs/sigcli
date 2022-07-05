ea146d075bc18e540a4f2a1364a3b2ebc6e2bb07
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-07-04 21:18:57 -0300

    adicionando o token aos headers da api #108

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+let token = localStorage.getItem('token');
+token = token.substring(1, token.length - 1);
+
 const api = axios.create({
   baseURL: "http://localhost:3001",
+  headers: {'Authorization': 'Bearer '+ token}
 });
 
 export default api;
