ed8c52d6add4a7eb9f5a7d91cfb5cd9f41c029ae
Adson Matheus <adson.matheus62@gmail.com>
2022-07-18 10:27:04 -0300

    test: teste do botao cancelar no formDoctor #176

--- client/src/components/Forms/FormDoctor/FormDoctor.test.js
+++ client/src/components/Forms/FormDoctor/FormDoctor.test.js
@@ -1,5 +1,6 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import FormDoctor from './FormDoctor';
+import { handleClickButtonSave } from '../../../pages/Doctor/Doctor';
 
 it("renders the form doctor's page", () => {
     render(<FormDoctor handleSaveButton={() => {}} />);
@@ -10,3 +11,28 @@ it("renders the form doctor's page", () => {
     expect(screen.getByLabelText("Phone:")).toBeInTheDocument();
     expect(screen.getByLabelText("Email:")).toBeInTheDocument();
 });
+
+it("clicks on the cancel button when creating a doctor", async () => {
+    render(<FormDoctor handleSaveButton={() => {handleClickButtonSave}} />);
+    const crm = screen.getByPlaceholderText("Ex.: 0000-UF");
+    const name = screen.getByPlaceholderText("Name");
+    const username = screen.getByPlaceholderText("Username");
+    const password = screen.getByPlaceholderText("Password");
+    const phone = screen.getByPlaceholderText("Phone");
+    const email = screen.getByPlaceholderText("Email");
+
+    crm.value = "7777-RN";
+    name.value = "Dr. João";
+    username.value = "joao";
+    password.value = "123";
+    phone.value = "84987745660";
+    email.value = "joao@gmail.com";
+
+    fireEvent.click(screen.getByText('Cancel'));
+    expect(crm.value).toEqual("");
+    expect(name.value).toEqual("");
+    expect(username.value).toEqual("");
+    expect(password.value).toEqual("");
+    expect(phone.value).toEqual("");
+    expect(email.value).toEqual("");
+})
